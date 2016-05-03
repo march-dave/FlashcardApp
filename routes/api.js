a246eff3a4c93e90e3a1151bc0e0aa445733e90d
march-dave <ysangy@gmail.com>
2016-05-02 17:49:31 -0700

    Ready to Mongo

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ var router = express.Router();
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
 // });
-router.use('/trees', require('./trees') );
+// router.use('/trees', require('./trees') );
+router.use('/flashcards', require('./flashcards') );
 
 module.exports = router;
